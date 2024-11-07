3e8f541fd71c20d3adb643b1aa1183b9e1ab4987
Björn Benouarets <bbenouarets@proton.me>
2024-11-07 15:23:54 +0100

    feat: Update build script

--- build.sh
+++ build.sh
@@ -19,5 +19,11 @@ GOOS=linux GOARCH=arm64 go build -o build/linux/arm64/$NAME
 GOOS=darwin GOARCH=amd64 go build -o build/darwin/amd64/$NAME
 GOOS=darwin GOARCH=arm64 go build -o build/darwin/arm64/$NAME
 
+cp build/linux/amd64/$NAME build/$NAME-linux_amd64
+cp build/linux/arm64/$NAME build/$NAME-linux_arm64
+cp build/windows/amd64/$NAME.exe build/$NAME-windows_amd64.exe
+cp build/darwin/amd64/$NAME build/$NAME-darwin_amd64
+cp build/darwin/arm64/$NAME build/$NAME-darwin_arm64
+
 # Done
 echo "Build completed!"
